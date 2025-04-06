d77bfbe09fa314a36a8f8842cf2d4f8aebd4114f
Adil Alahee <adilmdctg@gmail.com>
2025-04-06 13:10:56 +0600

    Practice with Mosh

---
+++ Practice With Mosh/app.py
@@ -0,0 +1,139 @@
+# Hello world
+# print ('Hello World')
+
+
+# Variable
+# age=24
+# age=35
+# print(age)
+
+# Input
+# first_name='Will Smith'
+# is_online=False
+# print(first_name,is_online)
+
+# name=input("whats your name?")
+# print("Hello "+ name)
+
+
+# name=input("whats your name?")
+# birth_year=input("Enter your birth year")
+# age=2020-int(birth_year)
+# print("Welcome "+ name)
+# print(age)
+
+
+# Type conversion
+# first=float(input("First Number:"))
+# second=float(input("Second number :"))
+# sum=first+second
+# print("Sum is "+str(sum))
+
+# String
+# course='Python for Beginners'
+# print(course.upper())
+# print(course.find('B'))
+# print('beginners' in course)
+# print(course.replace('B','CD'))
+# print(course)
+
+# Arithmetic operator
+# x=4
+# y=6
+# x-=1
+# y+=3
+# x=x//3
+# y=y%5
+# print(x,y)
+
+# Operator precedence
+# x=4*(2+6)
+# print(x)
+# y=(4//2)-8
+# print(y)
+
+# Comparison operator
+# x=5>2
+# y=5>=6
+# z=3<5
+# w=1<=0
+# a=2==2
+# b=1!=2
+# print(x,y,z,w,a,b)
+
+# Logical operator
+# price=33
+# print(price>20 and price < 25)
+# print(price>20 or price < 25)
+# print(not price<40)
+
+# IF statements
+# temperature=30
+# if temperature>40:
+#     print("It's a hot day")
+# elif temperature>30:
+#     print("Bit Cold")
+# else:
+#     print("Nice Day")
+
+# # Exercise
+# weight=int(input("Your Weight:"))
+# unit=input("'K' for KG and 'L' for LBS:")
+# if unit.upper()=='k':
+#     converted = weight /0.50
+#     print('Weight in KG'+ str(converted))
+# else:
+#     converted= weight * 0.50
+#     print('Weight in LBS'+str(converted))
+
+# While loops
+# i=1
+# while i<=5:
+#     print(i*'*')
+#     i=i+1
+
+# Lists
+# names=['Adil','Akil','Anika','Asif','Akib','Sathi']
+# print(names)
+# names[0]='Adills'
+# print(names[0])
+# print(names[0:4])
+
+# list methods
+# numbers=[1,2,3,4,5,6,7,8,9,0]
+# print(numbers)
+# numbers.append(11)
+# print(numbers)
+# numbers.insert(1,12)
+# print(numbers)
+# print(2 in numbers)
+# print(len(numbers))
+
+# For loops
+# numbers=[1,2,3,4,5,6,7,8,9,0]
+# for i in numbers:
+#     print(i)
+#
+# i=0
+# while i <len(numbers):
+#     print (numbers[i])
+#     i=i+1
+
+# Range function
+# numbers=range(9)
+# print(numbers)
+# for number in numbers:
+#     print(number)
+# numbers=range(2,10)
+# for i in numbers:
+#     print(i)
+# numbers=range(2,10,3)
+# for i in numbers:
+#     print(i)
+# for i in range(5):
+#     print(i)
+
+# Tuples
+# numbers=(1,2,3,4,5,6,7,8,9)
+# print(numbers)
+# print(numbers.index(7))
